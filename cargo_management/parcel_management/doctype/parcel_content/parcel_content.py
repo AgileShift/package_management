f72f774327886d8e7e6f83ebbaf1bf80a8af73d9
Jeans K. Real <jkrb7@hotmail.com>
2025-01-31 09:16:47 -0600

    Parcel Content now more text capable

--- cargo_management/parcel_management/doctype/parcel_content/parcel_content.py
+++ cargo_management/parcel_management/doctype/parcel_content/parcel_content.py
@@ -2,4 +2,23 @@ from frappe.model.document import Document
 
 
 class ParcelContent(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		amount: DF.Currency
+		description: DF.TextEditor
+		import_rate: DF.Float
+		item_code: DF.Link | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		qty: DF.Int
+		rate: DF.Currency
+		tracking_number: DF.Data | None
+	# end: auto-generated types
 	pass
